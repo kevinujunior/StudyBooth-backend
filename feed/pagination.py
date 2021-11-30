b265f8f368d9d1fd0506b0dd5ed2ed0666cd5144
kevinujunior <65278039+kevinujunior@users.noreply.github.com>
2021-11-30 12:48:09 +0530

    Update pagination.py

--- feed/pagination.py
+++ feed/pagination.py
@@ -8,7 +8,7 @@ class LargeResultsSetPagination(PageNumberPagination):
     max_page_size = 10000
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 12
     page_size_query_param = 'page_size'
     max_page_size = 1000
     

74584ae8fb9db48c8751cc42fd8c401f1ad87ea0
kevinujunior <65278039+kevinujunior@users.noreply.github.com>
2021-12-21 18:00:07 +0530

    ggdd

--- chat/models.py
+++ chat/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from users.models import User
 
 class Message(models.Model):
-    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE, null=True, blank=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 

3de2a43648f7de3062811acbe16765d645322912
kevinujunior <65278039+kevinujunior@users.noreply.github.com>
2022-01-06 12:30:06 +0530

    Update settings.py

--- studybooth/settings.py
+++ studybooth/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.openid',  
     'dj_rest_auth',
     'dj_rest_auth.registration',
     'corsheaders',

c562c94105739dcc6248c0e341057f910e953994
kevinujunior <65278039+kevinujunior@users.noreply.github.com>
2023-06-14 19:15:10 +0000

    m

--- studybooth/settings.py
+++ studybooth/settings.py
@@ -71,7 +71,7 @@ MEDIA_URL = '/media/'
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static') ,'backend']
 STATIC_URL = 'static/'

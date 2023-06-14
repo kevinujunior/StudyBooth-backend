3a4a31213501493e1fba00851cd3dbadcd43cf22
kevinujunior <65278039+kevinujunior@users.noreply.github.com>
2023-06-14 18:48:36 +0000

    hosts updated

--- studybooth/settings.py
+++ studybooth/settings.py
@@ -27,10 +27,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 DEBUG = 'RENDER' not in os.environ
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
